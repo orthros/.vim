7140a6dc6030e8a696ede849b199b0ab8d64923e
Colin Nelson <colin@orthros.dev>
2022-07-08 09:04:30 -0700

    feat(colorscheme): add transparency

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,12 @@
 local colorscheme = "tokyonight"
 
 vim.g.tokyonight_style = "night"
+-- Colorsheme transparency
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent_sidebar = true
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
-
-
