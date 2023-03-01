00d9d95113f7564a7adb860292c1b9a5c4618d27
Colin Nelson <colin@orthros.dev>
2023-03-01 12:39:08 -0800

    chore(nvim-tree): remove deprecated settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -36,12 +36,13 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
+  -- The *_on_setup options have been deprecated
+  -- open_on_setup = false,
+  -- ignore_ft_on_setup = {
+  --   "startify",
+  --   "dashboard",
+  --   "alpha",
+  -- },
   -- auto_close = true, -- This is now disabled
   open_on_tab = false,
   hijack_cursor = false,
@@ -78,8 +79,8 @@ nvim_tree.setup {
       custom_only = false,
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
+        { key = "h",                  cb = tree_cb "close_node" },
+        { key = "v",                  cb = tree_cb "vsplit" },
       },
     },
     number = false,
